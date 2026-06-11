4cf585ee383b812a494452cb74eedab1a74d9325
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-11 08:49:13 +0200

    ✨ feat(hosts): enable homebrew on smi-mac

--- hosts/smi-mac/default.nix
+++ hosts/smi-mac/default.nix
@@ -4,6 +4,7 @@
     host.name = "smi-mac";
     locale.timeZone = "Europe/Berlin";
     users.stateVersion = "26.05";
+    homebrew.enable = true;
   };
 
   nixpkgs.hostPlatform = "aarch64-darwin";
