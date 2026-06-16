671aa8b287235a713bfb0400dadf5d67ac3b34f1
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-16 23:30:28 +0200

    🎨 refactor(aerospace): rework bindings and add persistent workspaces

--- home/programs/utils/aerospace.nix
+++ home/programs/utils/aerospace.nix
@@ -1,103 +1,107 @@
 {
   smi.programs.aerospace.settings = {
+    after-startup-command = [ "layout tiles" ];
+
     start-at-login = true;
+
+    enable-normalization-flatten-containers = true;
+    enable-normalization-opposite-orientation-for-nested-containers = true;
+
+    accordion-padding = 30;
 
     default-root-container-layout = "tiles";
     default-root-container-orientation = "auto";
 
     on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
 
+    automatically-unhide-macos-hidden-apps = true;
+
+    persistent-workspaces = [
+      "1"
+      "2"
+      "3"
+      "4"
+      "5"
+      "6"
+      "7"
+      "8"
+      "9"
+    ];
+
+    on-mode-changed = [ ];
+
+    key-mapping.preset = "qwerty";
+
     gaps = {
-      inner.horizontal = 8;
-      inner.vertical = 8;
+      inner.horizontal = 15;
+      inner.vertical = 15;
       outer = {
-        left = 8;
-        bottom = 8;
-        top = 8;
-        right = 8;
+        left = 20;
+        bottom = 20;
+        top = 20;
+        right = 20;
       };
     };
 
     mode.main.binding = {
-      "alt-h" = "focus left";
-      "alt-j" = "focus down";
-      "alt-k" = "focus up";
-      "alt-l" = "focus right";
+      "cmd-h" = "focus left";
+      "cmd-j" = "focus down";
+      "cmd-k" = "focus up";
+      "cmd-l" = "focus right";
 
-      "alt-shift-h" = "move left";
-      "alt-shift-j" = "move down";
-      "alt-shift-k" = "move up";
-      "alt-shift-l" = "move right";
+      "cmd-shift-h" = "move left";
+      "cmd-shift-j" = "move down";
+      "cmd-shift-k" = "move up";
+      "cmd-shift-l" = "move right";
 
-      "alt-minus" = "resize smart -50";
-      "alt-equal" = "resize smart +50";
+      "cmd-shift-minus" = "resize smart -50";
+      "cmd-shift-equal" = "resize smart +50";
 
-      "alt-slash" = "layout tiles horizontal vertical";
-      "alt-comma" = "layout accordion horizontal vertical";
+      "shift-cmd-1" = "workspace 1";
+      "shift-cmd-2" = "workspace 2";
+      "shift-cmd-3" = "workspace 3";
+      "shift-cmd-4" = "workspace 4";
+      "shift-cmd-5" = "workspace 5";
+      "shift-cmd-6" = "workspace 6";
+      "shift-cmd-7" = "workspace 7";
+      "shift-cmd-8" = "workspace 8";
+      "shift-cmd-9" = "workspace 9";
 
-      "alt-f" = "fullscreen";
-      "alt-shift-space" = "layout floating tiling";
-
-      "alt-1" = "workspace 1";
-      "alt-2" = "workspace 2";
-      "alt-3" = "workspace 3";
-      "alt-4" = "workspace 4";
-      "alt-5" = "workspace 5";
-      "alt-6" = "workspace 6";
-      "alt-7" = "workspace 7";
-      "alt-8" = "workspace 8";
-      "alt-9" = "workspace 9";
-
-      "alt-shift-1" = "move-node-to-workspace 1";
-      "alt-shift-2" = "move-node-to-workspace 2";
-      "alt-shift-3" = "move-node-to-workspace 3";
-      "alt-shift-4" = "move-node-to-workspace 4";
-      "alt-shift-5" = "move-node-to-workspace 5";
-      "alt-shift-6" = "move-node-to-workspace 6";
-      "alt-shift-7" = "move-node-to-workspace 7";
-      "alt-shift-8" = "move-node-to-workspace 8";
-      "alt-shift-9" = "move-node-to-workspace 9";
-
-      "alt-tab" = "workspace-back-and-forth";
-      "alt-shift-tab" = "move-workspace-to-monitor --wrap-around next";
-
-      "alt-shift-c" = "reload-config";
-      "alt-shift-semicolon" = "mode service";
-    };
-
-    mode.service.binding = {
-      "esc" = [
-        "reload-config"
-        "mode main"
+      "shift-alt-cmd-1" = [
+        "move-node-to-workspace 1"
+        "workspace 1"
       ];
-      "r" = [
-        "flatten-workspace-tree"
-        "mode main"
+      "shift-alt-cmd-2" = [
+        "move-node-to-workspace 2"
+        "workspace 2"
       ];
-      "f" = [
-        "layout floating tiling"
-        "mode main"
+      "shift-alt-cmd-3" = [
+        "move-node-to-workspace 3"
+        "workspace 3"
       ];
-      "backspace" = [
-        "close-all-windows-but-current"
-        "mode main"
+      "shift-alt-cmd-4" = [
+        "move-node-to-workspace 4"
+        "workspace 4"
       ];
-
-      "alt-shift-h" = [
-        "join-with left"
-        "mode main"
+      "shift-alt-cmd-5" = [
+        "move-node-to-workspace 5"
+        "workspace 5"
       ];
-      "alt-shift-j" = [
-        "join-with down"
-        "mode main"
+      "shift-alt-cmd-6" = [
+        "move-node-to-workspace 6"
+        "workspace 6"
       ];
-      "alt-shift-k" = [
-        "join-with up"
-        "mode main"
+      "shift-alt-cmd-7" = [
+        "move-node-to-workspace 7"
+        "workspace 7"
       ];
-      "alt-shift-l" = [
-        "join-with right"
-        "mode main"
+      "shift-alt-cmd-8" = [
+        "move-node-to-workspace 8"
+        "workspace 8"
+      ];
+      "shift-alt-cmd-9" = [
+        "move-node-to-workspace 9"
+        "workspace 9"
       ];
     };
   };
