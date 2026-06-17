7a8ca33caa9e2401af6421116077a43f63856f81
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-17 19:26:34 +0200

    ✨ feat(vscodium): set up profiles, theme and minimal ui

--- home/programs/development/vscodium.nix
+++ home/programs/development/vscodium.nix
@@ -4,14 +4,113 @@
   pkgs,
   ...
 }:
+let
+  fromOpenVsx = pkgs.nix4vscode.forOpenVsx;
+  fromVscode = pkgs.nix4vscode.forVscode;
+in
 {
   config = lib.mkIf config.smi.programs.vscodium.enable {
     smi.programs.vscodium = {
-      extensions = with pkgs.vscode-extensions; [ ];
+      # Shared across every profile.
+      extensions = fromOpenVsx [
+        "antfu.theme-vitesse"
+        "miguelsolorio.symbols"
+        "editorconfig.editorconfig"
+        "aaron-bond.better-comments"
+        "mkhl.direnv"
+        "eamodio.gitlens"
+      ];
 
-      userSettings = { };
+      userSettings = {
+        "editor.fontFamily" = "JetBrains Mono";
+        "editor.fontSize" = 16;
+        "editor.lineHeight" = 1.8;
+        "editor.fontLigatures" = true;
 
-      profiles.default = { };
+        "editor.formatOnSave" = true;
+        "editor.smoothScrolling" = true;
+        "editor.cursorSmoothCaretAnimation" = "on";
+        "editor.cursorBlinking" = "smooth";
+
+        "workbench.colorTheme" = "Vitesse Dark";
+        "workbench.iconTheme" = "symbols";
+        "workbench.sideBar.location" = "left";
+        "workbench.activityBar.location" = "default";
+        "workbench.list.smoothScrolling" = true;
+
+        "terminal.integrated.fontFamily" = "JetBrainsMono Nerd Font";
+        "terminal.integrated.fontSize" = 14;
+        "terminal.integrated.smoothScrolling" = true;
+        "terminal.integrated.fontLigatures.enabled" = true;
+
+        "files.autoSave" = "onFocusChange";
+
+        "git.fetchOnPull" = true;
+        "telemetry.telemetryLevel" = "off";
+        "update.mode" = "none";
+        "extensions.autoUpdate" = false;
+
+        "editor.minimap.enabled" = false;
+        "breadcrumbs.enabled" = false;
+        "editor.stickyScroll.enabled" = false;
+        "editor.guides.indentation" = false;
+        "editor.renderWhitespace" = "none";
+        "editor.overviewRulerBorder" = false;
+        "editor.hideCursorInOverviewRuler" = true;
+        "window.commandCenter" = false;
+        "workbench.layoutControl.enabled" = false;
+        "workbench.startupEditor" = "none";
+        "workbench.tips.enabled" = false;
+      };
+
+      profiles = {
+        # Lightweight landing profile: shared only, no stack extensions.
+        default = { };
+
+        nix = {
+          extensions = fromOpenVsx [ "jnoortheen.nix-ide" ];
+          userSettings = {
+            "nix.enableLanguageServer" = true;
+            "nix.serverPath" = "nixd";
+            "[nix]"."editor.defaultFormatter" = "jnoortheen.nix-ide";
+          };
+        };
+
+        web = {
+          extensions = fromOpenVsx [
+            "dbaeumer.vscode-eslint"
+            "esbenp.prettier-vscode"
+            "bradlc.vscode-tailwindcss"
+          ];
+          userSettings = {
+            "[javascript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "[javascriptreact]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "[typescript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "[typescriptreact]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+            "[json]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+          };
+        };
+
+        infra = {
+          extensions =
+            fromOpenVsx [
+              "redhat.vscode-yaml"
+              "redhat.ansible"
+            ]
+            ++ fromVscode [ "pulumi.pulumi-vscode-tools" ];
+          userSettings = {
+            "redhat.telemetry.enabled" = false;
+          };
+        };
+
+        typst = {
+          extensions = fromOpenVsx [ "myriad-dreamin.tinymist" ];
+          userSettings = {
+            "[typst]"."editor.defaultFormatter" = "myriad-dreamin.tinymist";
+            "tinymist.formatterMode" = "typstyle";
+          };
+        };
+      };
     };
   };
 }
