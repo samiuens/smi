cdbfb94c5c42cb726c00f5a1320c12022df21d3b
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-17 19:26:34 +0200

    🔥 refactor(zed): drop declarative config, install via home.packages

--- home/programs/development/zed.nix
+++ home/programs/development/zed.nix
@@ -1,49 +1,11 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 {
   config = lib.mkIf config.smi.programs.zed.enable {
-    programs.zed-editor = {
-      extensions = [
-        "material-icon-theme"
-        "nix"
-      ];
-      userSettings = {
-        icon_theme = "Material Icon Theme";
-        ui_font_family = ".ZedSans";
-        ui_font_size = 16;
-
-        autosave = "on_focus_change";
-        cursor_shape = "block";
-        buffer_font_family = "JetBrains Mono";
-        buffer_font_size = 16;
-
-        terminal = {
-          font_family = "JetBrainsMono Nerd Font";
-          font_size = 14;
-        };
-
-        features.copilot = false;
-        disable_ai = true;
-
-        telemetry.metrics = false;
-
-        load_direnv = "shell_hook";
-        lsp.nixd.binary.path = "nixd";
-        languages.Nix = {
-          language_servers = [
-            "nixd"
-            "!nil"
-          ];
-          formatter.external = {
-            command = "nixfmt";
-            arguments = [ "-q" ];
-          };
-          format_on_save = "on";
-        };
-      };
-    };
+    home.packages = with pkgs; [ zed-editor ];
   };
 }
