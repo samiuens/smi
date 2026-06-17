b810f586155ec434934240ebf0dc75f23e5464ca
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2026-06-17 10:35:39 +0200

    🔧 chore(users): update smi program config

--- users/smi.nix
+++ users/smi.nix
@@ -14,65 +14,49 @@
             name = "Sami Arda Ünsay";
             email = "192653549+samiuens@users.noreply.github.com";
           };
-          git = {
+          github = {
             enable = true;
-            signing.enable = true;
-            user = {
-              name = "Sami Arda Ünsay";
-              email = "192653549+samiuens@users.noreply.github.com";
-            };
-            github = {
-              enable = true;
-              username = "samiuens";
-            };
+            username = "samiuens";
           };
-          lazygit.enable = true;
-          direnv.enable = true;
-          comma.enable = true;
-          claude.enable = true;
-          ghostty.enable = true;
-          keepassxc.enable = true;
-          drawio.enable = true;
-          kicad.enable = true;
-          #bitwarden.enable = true;
-          obsidian.enable = true;
-          #opencode.enable = true;
-          #bun.enable = true;
-          #vesktop.enable = true;
-          #zotero.enable = true;
         };
         lazygit.enable = true;
         direnv.enable = true;
         comma.enable = true;
         claude.enable = true;
-        ghostty.enable = true;
+        #ghostty.enable = true;
         keepassxc.enable = true;
-        chromium.enable = true;
+        drawio.enable = true;
+        #kicad.enable = true;
+        obsidian.enable = true;
+        # bitwarden.enable = true;
+        # opencode.enable = true;
+        # bun.enable = true;
+        # vesktop.enable = true;
+        # zotero.enable = true;
+        #chromium.enable = true;
         aerospace.enable = true;
         raycast.enable = true;
-        zen = {
-          enable = true;
-          spaces = {
-            "Persönlich" = {
-              id = "0597770a-4614-4634-8b27-63a4407577f1";
-              icon = "👤";
-              position = 1000;
-              container = 1;
+        /*
+          zen = {
+            enable = true;
+            spaces = {
+              "Persönlich" = {
+                id = "0597770a-4614-4634-8b27-63a4407577f1";
+                icon = "👤";
+                position = 1000;
+                container = 1;
+              };
+            };
+            containers = {
+              Personal = {
+                color = "blue";
+                icon = "circle";
+                id = 1;
+              };
             };
           };
-          containers = {
-            Personal = {
-              color = "blue";
-              icon = "circle";
-              id = 1;
-            };
-          };
-        };
+        */
       };
-
-      services.flatpak.packages = [
-        "com.bambulab.BambuStudio"
-      ];
     };
   };
 }
